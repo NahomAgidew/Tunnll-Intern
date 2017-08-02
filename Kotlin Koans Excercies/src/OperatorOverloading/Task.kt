c95ba9700eafa214b5180b9b7435625e2ed99662
Nahom Abi <nahomagidew@gmail.com>
2017-08-02 00:53:58 -0700

    Completed Operator Overloading task of Kotlin Koans

---
+++ Kotlin Koans Excercies/src/OperatorOverloading/Task.kt
@@ -0,0 +1,34 @@
+package OperatorOverloading
+
+import java.util.Calendar
+import ForLoop.MyDate
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+fun MyDate.addTimeIntervals(timeInterval: TimeInterval, number: Int): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth)
+    when (timeInterval) {
+        TimeInterval.DAY -> c.add(Calendar.DAY_OF_MONTH, number)
+        TimeInterval.WEEK -> c.add(Calendar.WEEK_OF_MONTH, number)
+        TimeInterval.YEAR -> c.add(Calendar.YEAR, number)
+    }
+    return MyDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DATE))
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) = addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
+
+fun task1(today: MyDate): MyDate {
+    return today + TimeInterval.YEAR + TimeInterval.WEEK
+}
+
+fun task2(today: MyDate): MyDate {
+    return today + TimeInterval.YEAR * 2 + TimeInterval.WEEK * 3 + TimeInterval.DAY * 5
+}
