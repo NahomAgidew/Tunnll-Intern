81e9180202f235cf298c4398145e25e76ec6c486
Nahom Abi <nahomagidew@gmail.com>
2017-08-02 10:21:51 -0700

    Completed omg-android

--- omg-android-starter/OMGAndroid/src/main/java/com/example/omgandroid/DetailActivityKotlin.kt
+++ omg-android-starter/OMGAndroid/src/main/java/com/example/omgandroid/DetailActivityKotlin.kt
@@ -1,11 +1,58 @@
 package com.example.omgandroid
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ImageView
+import android.widget.ShareActionProvider
+import com.squareup.picasso.Picasso
 
 class DetailActivityKotlin : Activity() {
 
+    private val IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"
+    internal var mImageURL = ""
+    internal var mSharedActionProvider: ShareActionProvider? = null
+
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
+
+        setContentView(R.layout.activity_detail)
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val imageView = findViewById(R.id.img_cover) as ImageView
+
+        val coverId = this.intent.extras.getString("coverID")
+
+        val len = coverId?.length ?: 0
+
+        if (len > 0) {
+            mImageURL = IMAGE_URL_BASE + coverId + "-L.jpg"
+            Picasso.with(this).load(mImageURL).placeholder(R.drawable.img_books_loading).into(imageView)
+        }
+    }
+
+    private fun setShareIntent() {
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Book Recommendation!")
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL)
+
+        mSharedActionProvider?.setShareIntent(shareIntent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        val shareItem = menu.findItem(R.id.menu_item_share)
+
+        mSharedActionProvider = shareItem!!.actionProvider as ShareActionProvider
+
+        setShareIntent()
+
+        return true
     }
 }
