e21f2c7dcde9d514d2807992c83039b27ee775d2
Nahom Abi <nahomagidew@gmail.com>
2017-08-02 09:06:46 -0700

    Completed Properties task of Kotlin Koans.

---
+++ Kotlin Koans Excercies/src/Properties/Task.kt
@@ -0,0 +1,10 @@
+package Properties
+
+class PropertyExample() {
+    var counter = 0
+    var propertyWithCounter: Int? = null
+        set(v: Int?) {
+            field = v
+            counter++
+        }
+}
