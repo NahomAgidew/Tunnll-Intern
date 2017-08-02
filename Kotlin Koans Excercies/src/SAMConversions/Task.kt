0a60f4a99a29be2b48d886b5e0e17bd9774e8ed0
Nahom Abi <nahomagidew@gmail.com>
2017-08-01 23:48:15 -0700

    Completed SAMConversions task of Kotlin Koans

---
+++ Kotlin Koans Excercies/src/SAMConversions/Task.kt
@@ -0,0 +1,9 @@
+package SAMConversions
+
+import java.util.*
+
+fun getList(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, { x, y -> y - x })
+    return arrayList
+}
