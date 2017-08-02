7b947fb03f3c415ff4c736ed0dea1915cbdc49eb
Nahom Abi <nahomagidew@gmail.com>
2017-08-02 09:54:11 -0700

    Downloaded omg-android-starter project

---
+++ omg-android-starter/OMGAndroid/src/main/java/com/example/omgandroid/DetailActivityKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.omgandroid
+
+import android.app.Activity
+import android.os.Bundle
+
+class DetailActivityKotlin : Activity() {
+
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
+    }
+}
